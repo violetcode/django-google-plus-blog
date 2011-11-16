ce004a7410a46b09dcd2693fac516dc862fef3ae
violet <marie@violetco.de>
2011-11-15 21:11:43 -0800

    no flashbang

--- lavender/urls.py
+++ lavender/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-	(r'^', include('lavender.flashbang.urls')),
+	(r'^', include('lavender.violetplus.urls')),
     # Examples:
     # url(r'^$', 'lavender.views.home', name='home'),
     # url(r'^lavender/', include('lavender.foo.urls')),
