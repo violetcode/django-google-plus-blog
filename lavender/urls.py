df74f706931fab4d93035fbec20ce4637a6f3230
Tyler <underlost@gmail.com>
2011-11-17 10:37:06 -0600

    html stuff

--- lavender/urls.py
+++ lavender/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^/$', direct_to_template, {"template": "home.html"}, name="home"),    
+    url(r'^/?$', direct_to_template, {"template": "home.html"}, name="home"),    
 	url(r'^', include('lavender.violetplus.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
